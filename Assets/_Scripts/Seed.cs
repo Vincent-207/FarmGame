ea92e23979ad20b9e58c5b4e7f9bef95cac8c5d6
Vincent-207 <vhannah2007@outlook.com>
2025-12-30 19:25:40 -0800

    Seeds & organization

---
+++ Assets/_Scripts/Seed.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Seed : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int growthStages;
+    public float growthDuration;
+    [SerializeField]
+    float currentGrowthTime;
+    [SerializeField]
+    int currentGrowthStage;
+    [SerializeField]
+    Gradient growthGradient;
+    SpriteRenderer seedRenderer;
+    void Start()
+    {
+        currentGrowthTime = 0;
+        seedRenderer = GetComponent<SpriteRenderer>();
+        
+    }
+
+    Gradient CreateGrowthGradient()
+    {
+        Gradient output = new Gradient();
+        return output;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentGrowthTime += Time.deltaTime;
+        currentGrowthStage = (int) (10 * currentGrowthTime/growthDuration);
+        seedRenderer.color = growthGradient.Evaluate(currentGrowthStage/(float) growthStages);
+
+    }
+}
