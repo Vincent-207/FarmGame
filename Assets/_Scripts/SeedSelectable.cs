c991028a487fe35aa788c0efbe0298add6f49546
Vincent-207 <vhannah2007@outlook.com>
2026-01-05 01:03:58 -0600

    Visitor and shop system

---
+++ Assets/_Scripts/SeedSelectable.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SeedSelectable : MonoBehaviour
+{
+    int quantity;
+    GameObject seed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int a = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
